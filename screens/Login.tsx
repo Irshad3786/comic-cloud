aa60dc54037e426d72abb6dd63bd4fac31ffc76e
irshad3786 <irshad9182278505@gmail.com>
2026-08-02 00:55:55 +0530

    created shadow effect

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -10,7 +10,7 @@ const Login = () => {
       <Text  className=" text-[#0058BE] text-4xl   text-center pt-4 font-anybody-bolditalic ">COMIC CLOUD</Text>
       <Image
       source={require("../assets/images/comic.png")}
-      className="w-96 h-96 self-center rounded-3xl mt-5 border-black border-4"
+      className="w-96 h-72 self-center rounded-3xl mt-5 border-black border-2"
       resizeMode="cover"
     />
     <Text  className=" text-[#0c0c0c] text-2xl text-center pt-1 font-anybody-bolditalic ">READY TO CREATE ?</Text>
@@ -25,19 +25,32 @@ const Login = () => {
     </View>
     <TextInput
       placeholder="Enter your password"
+      secureTextEntry
       className="w-[88%] h-14 ml-4 mt-2 bg-white  px-4 text-black border-black border-2"
     />
 
-    <Pressable
+<View className=" mt-6">
+  <View className="absolute top-2 left-7 w-[88%] h-14 bg-[#003f8c]" />
+
+  <Pressable
     onPress={() => console.log("Login")}
-    className="bg-blue-600 w-[88%] ml-4 mt-6 h-14  justify-center items-center active:opacity-80  border-black border-4"
+    className="bg-[#0058BE] left-5 w-[88%] h-14 justify-center items-center border-2 border-black"
   >
-    <Text className="text-white text-3xl font-anybody-bolditalic ">
+    <Text className="text-white text-3xl font-anybody-bolditalic">
       LOGIN
     </Text>
   </Pressable>
+    </View>
+
+    <View className="flex-row items-center justify-between mt-8 px-4">
+      <View className="h-[3px] bg-gray-400 flex-1" />
+      <Text className="mx-3 font-mono">OR CONTINUE WITH</Text>
+      <View className="h-[3px] bg-gray-400 flex-1" />
+    </View>
+        
 
     </SafeAreaView>
+    
   )
 }
 
